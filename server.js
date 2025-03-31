dff422dfc74451a640cc1ec03d0cfaeaeac2faf4
Nwosu Paul <nwosupaulchinagorom@gmail.com>
2025-03-31 16:07:35 +0100

    worked on the content header security

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
 dotenv.config({ path: "/config.env" });
-const userModel = require("./Models/userModel");
+const UserModel = require("./Models/userModel");
 const DB = process.env.DATABASE.replace(
   "<db_password>",
   process.env.DATABASE_PASSWORD
